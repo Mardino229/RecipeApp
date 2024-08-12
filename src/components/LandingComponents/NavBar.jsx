5c5c7ebfae255f76fbfd43a1b00a7bf205721652
Mardino229 <maxdassi01@gmail.com>
2024-08-12 18:55:50 +0200

    refactor

--- src/components/LandingComponents/NavBar.jsx
+++ src/components/LandingComponents/NavBar.jsx
@@ -11,7 +11,7 @@ export default function Navbar(){
     const links = [
         {
             name: "Home",
-            path: "/",
+            path: "/recipeapp/",
             icon: faHome
         },
         // {
@@ -21,7 +21,7 @@ export default function Navbar(){
         // },
         {
             name: "Login",
-            path: "/login",
+            path: "/recipeapp/login",
             icon: faRightToBracket
         }
 
