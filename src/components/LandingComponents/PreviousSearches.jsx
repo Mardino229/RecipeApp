7e4b66c455cf298164d5abdc97c42ecec4c59c65
Mardino229 <maxdassi01@gmail.com>
2024-09-21 16:30:48 +0200

    refactor

--- src/components/LandingComponents/PreviousSearches.jsx
+++ src/components/LandingComponents/PreviousSearches.jsx
@@ -6,13 +6,13 @@ export default function PreviousSearches({search}){
     const searches = ['pizza', 'burger', 'cookies', 'juice', 'biriyani', 'salad', 'ice cream', 'lasagna', 'pudding', 'soup']
     return (
         <div className="previous-searches section">
-            <h2>Previous Searches</h2>
-            <div className="previous-searches-container">
-                {searches.map((search, index) => (
-                    <div key={index} style={{animationDelay: index * .1 + "s"}} className="search-item">
-                        {search}
-                    </div>))}
-            </div>
+            {/*<h2>Previous Searches</h2>*/}
+            {/*<div className="previous-searches-container">*/}
+            {/*    {searches.map((search, index) => (*/}
+            {/*        <div key={index} style={{animationDelay: index * .1 + "s"}} className="search-item">*/}
+            {/*            {search}*/}
+            {/*        </div>))}*/}
+            {/*</div>*/}
             {/*<div className="search-box">*/}
             {/*    /!*<input type="text" placeholder="Search ..."/>*!/*/}
             {/*    /!*<button className="btn">*!/*/}
